3319e91bd078100fcce543fbec5037e298f06aba
juanma-ge <gejuan@iesrafaelalberti.es>
2024-09-27 19:04:21 +0200

    Ejercicios 1 al 5 y el README

---
+++ src/ejercicio1.py
@@ -0,0 +1,5 @@
+#Ejercicio 1 
+nombre = input("Escribe tu nombre: ")
+print(f"Hola, {nombre}.")
+
+
---
+++ src/ejercicio2.py
@@ -0,0 +1,8 @@
+#Ejercicio 2
+horas_de_trabajo = float(input("Dime las horas de trabajo: "))
+
+precio_por_hora = float(input("Dime el precio por hora: "))
+
+importe_total = horas_de_trabajo * precio_por_hora
+
+print(f"El importe total es: {importe_total}")
